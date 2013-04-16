403c24f2f658903676062c0e997f429bb178d48a
Antonio Ono <me@antonioono.com>
2013-04-15 20:55:10 -0400

    fallback to non-middleware static file serving

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 var express = require('express'),
-    app = express();
+    fs = require('fs');
+
+var app = express();
     
-app.use(express.static(__dirname + '/static'));
+// app.use(express.static(__dirname + '/static'));
+
+app.get("/static/:staticFilename", function (req, res) {
+  res.sendfile("static/" + req.params.staticFilename);
+});
     
 app.get('/', function(req, res){
     res.send('test');
