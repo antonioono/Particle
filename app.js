e655ba9d85f75d4bc443f932b6299a64011f2713
Antonio Ono <me@antonioono.com>
2013-04-15 21:08:37 -0400

    routes

--- app.js
+++ app.js
@@ -3,11 +3,23 @@ var express = require('express'),
 
 var app = express();
     
-// app.use(express.static(__dirname + '/static'));
+app.use(express.static(__dirname + '/static'));
 
 app.get("/static/:staticFilename", function (req, res) {
   res.sendfile("static/" + req.params.staticFilename);
 });
+
+app.get("/static/scripts/:staticFilename", function (req, res) {
+  res.sendfile("static/scripts/" + req.params.staticFilename);
+});
+
+app.get("/static/style/:staticFilename", function (req, res) {
+  res.sendfile("static/style/" + req.params.staticFilename);
+});
+
+// app.get("/static/:staticFilename", function (req, res) {
+//   res.sendfile("static/" + req.params.staticFilename);
+// });
     
 app.get('/', function(req, res){
     res.send('test');
