c7a6e430e477732a634d5e80300c6bad8e7b08b4
Victor Song <victor@victor-song.com>
2013-05-01 19:21:24 -0400

    top and left snapping

--- Static/scripts/particle.js
+++ Static/scripts/particle.js
@@ -57,7 +57,7 @@ $(document).ready(function(){
             "source":"http://placekitten.com/310"
         },*/
     ]
-    
+
     function makeoffsets() {
         for (var i = 0; i < data.length; i++) {
             var container = $("<article>");
@@ -66,11 +66,11 @@ $(document).ready(function(){
                     src: data[i].source
                 })
             }
-            
+
             $("#space").append(container.append(content));
         }
     }
-    
+
     for (var i = 0; i < data.length; i++) {
     }
     makeoffsets();
@@ -78,28 +78,64 @@ $(document).ready(function(){
 })
 
 function drag() {
-    var isDrag, dragged;
-    
+    var dragging, draggedItem;    
     var item = $("article");
-        
+
     $(item).mousedown(function(event){
-        isDrag = true;
-        dragged = $(this);
+        dragging = true;
+        draggedItem = $(this);
         event.preventDefault();
     })
-    
+
     $(document).mousemove(function(event){
-        if (isDrag) {
+        if (dragging) {
             var offset = $(item).offset();
-            $(dragged).offset({
+            $(draggedItem).offset({
                 top:  event.pageY,
                 left: event.pageX
             })
-        snap(dragged);
         }
     })
-    
+
     $(document).mouseup(function(){
-        isDrag = false;
+        dragging = false;
+        snap(draggedItem);
     })
+}
+
+function snap(item) {
+    isSnapLegal(item);
+}
+
+function isSnapLegal(item) {
+    var d    = item.position(),
+        allD = [];
+    $("article").not(item).each(function(){
+        allD.push($(this).position());
+    })
+    //find top left bottom right cx cy of present objects
+    for (i in allD) {
+        // console.log(allD[0]);
+        comparePos(item.position().top,    allD[i].top, "top",   item);
+        comparePos(item.position().left,   allD[i].left, "left", item);
+        // comparePos(item.position().right,  allD[i].right);
+        // comparePos(item.position().bottom, allD[i].bottom);
+    }
+}
+
+function comparePos(a, b, side, item) {
+    var snapThreshold = 24;
+    // console.log(a, b);
+    //compare distances, if distance < 10px, snap
+    if (Math.abs(a-b) < snapThreshold) {
+        // console.log("snap!");
+        snapAction(b, side, item);
+    }
+}
+
+function snapAction(a, side, item) {
+    //changes top or left or both of selected object
+    console.log(item.position()[side]);
+    item.css(side, a);
+    console.log(item.css(side));
 }
