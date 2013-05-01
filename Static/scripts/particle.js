966a948df2d4d255cba70e71ab9a70d458ba64a1
Victor Song <victor@victor-song.com>
2013-05-01 16:48:05 -0400

    snap attempt
    failed.

--- Static/scripts/particle.js
+++ Static/scripts/particle.js
@@ -8,7 +8,7 @@ $(document).ready(function(){
             "type":"image",
             "source":"http://placekitten.com/310"
         },
-        {
+/*        {
             "type":"image",
             "source":"http://placekitten.com/233"
         },
@@ -55,10 +55,10 @@ $(document).ready(function(){
         {
             "type":"image",
             "source":"http://placekitten.com/310"
-        },
+        },*/
     ]
     
-    function makeItems() {
+    function makeoffsets() {
         for (var i = 0; i < data.length; i++) {
             var container = $("<article>");
             if (data[i].type === "image") {
@@ -73,23 +73,23 @@ $(document).ready(function(){
     
     for (var i = 0; i < data.length; i++) {
     }
-    makeItems();
+    makeoffsets();
     drag();
 })
 
 function drag() {
-    var drag, dragged;
+    var isDrag, dragged;
     
     var item = $("article");
         
     $(item).mousedown(function(event){
-        drag = true;
+        isDrag = true;
         dragged = $(this);
         event.preventDefault();
     })
     
     $(document).mousemove(function(event){
-        if (drag) {
+        if (isDrag) {
             var offset = $(item).offset();
             $(dragged).offset({
                 top:  event.pageY,
@@ -100,35 +100,74 @@ function drag() {
     })
     
     $(document).mouseup(function(){
-        drag = false;
+        isDrag = false;
     })
 }
 
 function snap(item) {
-    var offsets= [];    
+    var offsets = []; 
+// finds the dimensions of the item
     var offset = {
         top: $(item).offset().top,
         left: $(item).offset().left,
-        bottom: $(item).offset().top  + $(item).height(),
-        right: $(item).offset().left + $(item).width()
-    }    
+        bottom: $(item).offset().top   + $(item).height(),
+        right: $(item).offset().left   + $(item).width(),
+        centerX: $(item).offset().left + ($(item).width() / 2),
+        centerY: $(item).offset().top  + ($(item).height() / 2) 
+    }
+// finds the dimensions of exisiting offsets
     $("article").each(function(){
         var distance = {
             top: $(this).offset().top,
             left: $(this).offset().left,
-            bottom: $(this).offset().top  + $(this).height(),
-            right: $(this).offset().left + $(this).width()
+            bottom: $(this).offset().top   + $(this).height(),
+            right: $(this).offset().left   + $(this).width(),
+            centerX: $(this).offset().left + ($(this).width() / 2),
+            centerY: $(this).offset().top  + ($(this).height() / 2)
         }
         offsets.push(distance);
     })
-    
-    
-// find mouse position
-// find position and dimensions of dragged object
-// if any side is within distance, change position of dragged object
-// if mouse position moves more than distance away from initial snap, unsnap object
+    findSnap(offset, offsets, item);
 }
 
-function isInRange(item, array){
-    
+// snaps if the item is in range of existing item
+function findSnap(offset, offsets, item) { 
+    var distance = 10;
+    for (var i = 0; i < offsets.length; i++) {
+        if (isInRange(offsets[i].top, offset.bottom, distance)) {
+            isDrag = false;
+            $(item).offset({
+                top: offsets[i].top - $(item).height()
+            })
+            console.log($(item).offset().top)
+        }
+        if (isInRange(offsets[i].left, offset.right, distance)) {
+            isDrag = false;
+            $(item).offset({
+                left: offsets[i].left - $(item).width()
+            })
+        }
+        if (isInRange(offsets[i].bottom, offset.top, distance)) {
+            isDrag = false;
+            $(item).offset({
+                top: offsets[i].bottom 
+            })
+        }
+        if (isInRange(offsets[i].right, offset.left, distance)) {
+            isDrag = false;
+            $(item).offset({
+                left: offsets[i].right
+            })
+        }
+    }
+}
+
+function isInRange(a, b, distance) {
+    if ((a - b) <= distance) {
+        return true;
+    } else if ((b - a) <= distance) {
+        return true;
+    } else {
+        return false;
+    }
 }
